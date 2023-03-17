2e5c9a784246b8e096f76ad1203e6a96322c9eec
Eric Onyango <eric.onyango@student.moringaschool.com>
2023-03-17 08:40:48 +0300

    Styling updates

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function BlogList() {
   const [blogs, setBlogs] = useState([]);
@@ -11,7 +12,7 @@ function BlogList() {
 
   const fetchBlogs = async () => {
     try {
-      const response = await fetch('http://localhost:3000/api/v1/blogs');
+      const response = await fetch("http://localhost:3000/api/v1/blogs");
       const data = await response.json();
       setBlogs(data);
     } catch (error) {
@@ -19,17 +20,17 @@ function BlogList() {
     }
   };
 
-  const handleUpdate = async id => {
+  const handleUpdate = async (id) => {
     try {
       const response = await fetch(`http://localhost:3000/api/v1/blogs/${id}`, {
-        method: 'PATCH',
+        method: "PATCH",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(updatedBlog),
       });
       const data = await response.json();
-      const updatedBlogs = blogs.map(blog => {
+      const updatedBlogs = blogs.map((blog) => {
         if (blog.id === data.id) {
           return data;
         } else {
@@ -39,25 +40,25 @@ function BlogList() {
       setBlogs(updatedBlogs);
       setSelectedBlog(null);
       setUpdatedBlog({});
-      alert('Blog post updated!');
+      alert("Blog post updated!");
     } catch (error) {
       console.log(error);
     }
   };
 
-  const handleDelete = async id => {
+  const handleDelete = async (id) => {
     try {
       await fetch(`http://localhost:3000/api/v1/blogs/${id}`, {
-        method: 'DELETE',
+        method: "DELETE",
       });
-      setBlogs(blogs.filter(blog => blog.id !== id));
-      alert('Blog post deleted!');
+      setBlogs(blogs.filter((blog) => blog.id !== id));
+      alert("Blog post deleted!");
     } catch (error) {
       console.log(error);
     }
   };
 
-  const handleEdit = blog => {
+  const handleEdit = (blog) => {
     setSelectedBlog(blog);
     setUpdatedBlog({
       title: blog.title,
@@ -67,7 +68,7 @@ function BlogList() {
     });
   };
 
-  const handleInputChange = event => {
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUpdatedBlog({ ...updatedBlog, [name]: value });
   };
@@ -75,46 +76,92 @@ function BlogList() {
   return (
     <div>
       <h2>Blog List</h2>
-      <ul>
+      <ListGroup>
         {blogs &&
-          blogs.map(blog => (
-            <li key={blog.id}>
-              <h3>{blog.title}</h3>
-              <p>Category: {blog.category}</p>
-              <p>Text: {blog.text}</p>
-              <p>Author: {blog.author}</p>
-              <button onClick={() => handleDelete(blog.id)}>Delete</button>
-              <button onClick={() => handleEdit(blog)}>Edit</button>
-            </li>
+          blogs.map((blog) => (
+            <ListGroup.Item key={blog.id} variant="light">
+              <div>
+                <h3>{blog.title}</h3>
+                <p>Category: {blog.category}</p>
+                <p>Text: {blog.text}</p>
+                <p>Author: {blog.author}</p>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(blog.id)}
+                >
+                  Delete
+                </button>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => handleEdit(blog)}
+                >
+                  Edit
+                </button>
+              </div>
+            </ListGroup.Item>
           ))}
-      </ul>
+      </ListGroup>
       {selectedBlog && (
-  <form>
-    <h2>Edit Blog Post</h2>
-    <label>
-      Title:
-      <input type="text" name="title" value={updatedBlog.title} onChange={handleInputChange} />
-    </label>
-    <br />
-    <label>
-      Category:
-      <input type="text" name="category" value={updatedBlog.category} onChange={handleInputChange} />
-    </label>
-    <br />
-    <label>
-      Text:
-      <input type="text" name="text" value={updatedBlog.text} onChange={handleInputChange} />
-    </label>
-    <br />
-    <label>
-      Author:
-      <input type="text" name="author" value={updatedBlog.author} onChange={handleInputChange} />
-    </label>
-    <br />
-    <button type="button" onClick={() => handleUpdate(selectedBlog.id, updatedBlog)}>Save</button>
-    <button type="button" onClick={() => setSelectedBlog(null)}>Cancel</button>
-  </form>
-)}
-</div>)}
+        <form>
+          <h2>Edit Blog Post</h2>
+          <label>
+            Title:
+            <input
+              type="text"
+              name="title"
+              value={updatedBlog.title}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Category:
+            <input
+              type="text"
+              name="category"
+              value={updatedBlog.category}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Text:
+            <input
+              type="text"
+              name="text"
+              value={updatedBlog.text}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Author:
+            <input
+              type="text"
+              name="author"
+              value={updatedBlog.author}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <button
+            className="btn btn-success"
+            type="button"
+            onClick={() => handleUpdate(selectedBlog.id, updatedBlog)}
+          >
+            Save
+          </button>
+          <button
+            className="btn btn-secondary"
+            type="button"
+            onClick={() => setSelectedBlog(null)}
+          >
+            Cancel
+          </button>
+        </form>
+      )}
+    </div>
+  );
+}
 
-export default BlogList
+export default BlogList;
